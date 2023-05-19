506063c370ece4f6ec5042580a4a8b9c12729969
Kiko Carisse <kiko@mediabot.ca>
2023-05-18 21:37:34 -0600

    creating global loading spinner

--- devops/deploy-updates-to-ubuntu-server.sh
+++ devops/deploy-updates-to-ubuntu-server.sh
@@ -13,6 +13,9 @@ systemctl restart lightningplm.service
 # rebuild and restart angular app
 cd /home/ubuntu/apps/lightningplm-app/lightning-plm/client
 npm i
-ng build
+ng build --output-path dist-new/lightning-plm
+#swap new and old dist folders
+mv dist dist-old && mv dist-new dist
+rm -rf dist-old
 
 systemctl restart nginx
