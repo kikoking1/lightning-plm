3cc5b14d04a59fcb9d7dc718af5aba15ca6c73dd
Kiko Carisse <kiko@mediabot.ca>
2023-05-20 15:26:51 -0600

    converting navbar to material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { ProductAddComponent } from './components/products/product-add/product-a
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { ProductEditComponent } from './components/products/product-edit/product-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -32,6 +36,10 @@ import { ProductEditComponent } from './components/products/product-edit/product
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [
     {
