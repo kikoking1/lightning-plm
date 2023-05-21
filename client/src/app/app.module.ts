8eb793ca0b6ba99a323d5becc7b98a5d0a11c718
Kiko Carisse <kiko@mediabot.ca>
2023-05-20 22:52:14 -0600

    hooking up mobile nav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -40,6 +42,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatSidenavModule,
+    MatDividerModule,
   ],
   providers: [
     {
